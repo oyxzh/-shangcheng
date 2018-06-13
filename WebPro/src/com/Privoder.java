61bdfa5660409c5b98c5a3b5895a844651a6afdb
oyxzh <ouyangxiongzhi@sina.com>
2018-06-13 10:41:19 +0800

    test idea

---
+++ WebPro/src/com/Privoder.java
@@ -0,0 +1,7 @@
+package WebPro.src.com;
+
+public class Privoder {
+    public  static  String sendMsg5(){
+        return  "hello app";
+    }
+}
---
+++ WebPro/src/com/Test1.java
@@ -0,0 +1,9 @@
+package WebPro.src.com;
+
+public class Test1 {
+
+    private  String name;
+    public static void main(String[] args) {
+        Privoder.sendMsg5();
+    }
+}
